c6819b2a8cde23028027e371d017e49a31f69f1b
Jiyun Kim <jyjyjy7418@gmail.com>
2023-08-04 20:54:02 +0900

    [Bronze II] Title: 막대기, Time: 88 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -4,14 +4,10 @@ N = int(sys.stdin.readline())
 stack = [int(sys.stdin.readline()) for _ in range(N)]
 
 cnt = 0
-standard = stack[-1]
-for i in reversed(range(N)):
-    if cnt == 0:
+standard = 0
+for s in reversed(stack):
+    if s > standard:
+        standard = s
         cnt += 1
-        continue
-    else:
-        if standard < stack[i]:
-            standard = stack[i]
-            cnt += 1
 
 print(cnt)
