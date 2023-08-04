28f3b21916bbc016f2cc5507650878d56e2a8401
Jiyun Kim <jyjyjy7418@gmail.com>
2023-08-04 20:06:53 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 36 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
@@ -1,13 +1,6 @@
 N = int(input())
 
-array = []
-for i in range(N):
-    array.append(list(input().split()))
-
-for i, a in enumerate(array):
-    a.reverse()
-    print(f'Case #{i+1}:', end=' ')
-    for i in a:
-        print(i, end=' ')
-    print()
-    
+stack = [input().split() for _ in range(N)]
+for i, s in enumerate(stack):
+    result = ' '.join(s[::-1])
+    print(f'Case #{i+1}: {result}')
