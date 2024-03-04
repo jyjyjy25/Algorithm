8e2a49b61de1a9b66cb9c117d520b312e94900d1
Jiyun Kim <jyjyjy7418@gmail.com>
2024-03-04 22:35:53 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,14 +1,11 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
-coin_list = [int(sys.stdin.readline().strip()) for _ in range(N)]
+A = [int(sys.stdin.readline().rstrip()) for _ in range(N)]
 
 cnt = 0
-rest_money = K
-for c in coin_list[::-1]:
-    cnt += rest_money // c
-    rest_money -= (c * (rest_money // c))
-    if rest_money == 0:
-        break
+for a in reversed(A):
+    cnt += K // a
+    K %= a  
 
 print(cnt)
