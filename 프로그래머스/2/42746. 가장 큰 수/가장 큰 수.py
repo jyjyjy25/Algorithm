d84d673d6ccc42edbea981cf79712b2016d5b429
Jiyun Kim <jyjyjy7418@gmail.com>
2024-03-16 00:46:13 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = ''
+    
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    answer = ''.join(numbers)
+        
+    return str(int(answer))
