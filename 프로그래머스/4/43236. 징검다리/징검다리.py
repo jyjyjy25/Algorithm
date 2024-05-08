23335ff67be2a194eefec738f68d2d99a9f587bf
Jiyun Kim <jyjyjy7418@gmail.com>
2024-05-09 00:46:19 +0900

    [level 4] Title: 징검다리, Time: 102.95 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -0,0 +1,30 @@
+def solution(distance, rocks, n):
+    answer = 0
+    
+    rocks.sort()
+    rocks.append(distance)
+    
+    low = 1
+    high = distance
+    while (low <= high):
+        mid = (low + high) // 2
+        
+        delete = 0  # 삭제한 바위 수
+        prev_rock = 0  # 이전 바위 위치
+        
+        for rock in rocks:
+            if rock - prev_rock < mid:  # 거리가 커트라인보다 작으면 바위 삭제 (왜냐면 최댓값을 구하는게 목적)
+                delete += 1
+            else:
+                prev_rock = rock
+            
+            if delete > n:  # 삭제한 바위 수가 n보다 크면 break
+                break
+        
+        if delete <= n:
+            answer = mid  # 최댓값 저장
+            low = mid + 1  # 최댓값을 구하기 위해 커트라인 재설정
+        else:
+            high = mid - 1
+                
+    return answer
