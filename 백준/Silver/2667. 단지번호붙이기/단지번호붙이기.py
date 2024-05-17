54f3450574535b8bb574585e2eb645c0a7babe15
Jiyun Kim <jyjyjy7418@gmail.com>
2024-05-18 00:39:22 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,37 +1,39 @@
 import sys
-input = sys.stdin.readline
 
-dx=[-1,0,1,0]
-dy=[0,1,0,-1]
-n=int(input().rstrip())
-matrix=[list(input().rstrip()) for _ in range(n)]
+N = int(sys.stdin.readline())
 
-cnt = 0
-result=[]
+house_num = []
+visited = [[False] * N for _ in range(N)]
 
-def dfs(x,y) :
+maps = []
+for _ in range(N):
+    maps.append(list(map(int, sys.stdin.readline().rstrip())))
+
+# 상하좌우
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+
+def DFS(x, y):
     global cnt
-    matrix[x][y] = "0" #방문한 곳 0으로 만들기
-    cnt+=1 #카운트 수 늘리기
-    for i in range(4) : #상하좌우로 움직이기
-        nx = x +dx[i]
-        ny = y +dy[i]
-        if nx < 0 or nx >= n or ny < 0 or ny >= n :
-            #만약 움직인 좌표가 범위를 벗어났다면 continue
-            continue
-        if matrix[nx][ny] == "1" :
-            #움직인 좌표에 집이 있다면 다시 dfs탐색
-            dfs(nx,ny)
+    visited[x][y] = True
+    cnt += 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < N:  # 지도 밖을 벗어나지 않는지 확인
+            if maps[nx][ny] and not visited[nx][ny]:  # 집이 있고 방문한 적이 없는 경우
+                DFS(nx, ny)
 
-for i in range(n) :
-    for j in range(n) :
-        #이중 for문으로 matrix를 하나하나 돌으면서 1인 구간 찾기(시작점 찾기)
-        if matrix[i][j]=="1" :
-            cnt = 0 #시작 전 카운트 초기화
-            dfs(i,j)
-            result.append(cnt) #탐색 끝난 후 카운트 값 저장
 
-print(len(result))
-result.sort()
-for c in result :
-    print(c)
+for x in range(N):
+    for y in range(N):
+        if maps[x][y] and not visited[x][y]:
+            cnt = 0
+            DFS(x, y)
+            if cnt != 0:
+                house_num.append(cnt)
+
+print(len(house_num))
+for h in sorted(house_num):
+    print(h)
