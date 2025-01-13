9409b96cff23a13c03be9b12b61ffe6436040ffc
Jiyun Kim <jyjyjy7418@gmail.com>
2025-01-14 01:03:21 +0900

    [level 2] Title: 주식가격, Time: 35.09 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,13 +1,14 @@
 def solution(prices):
-    answer = []
+    stack = []
+    answer = [0] * len(prices)
     
-    for i, p in enumerate(prices):
-        seconds = 0
-        for j in range(i+1, len(prices)):
-            seconds += 1
-            if p > prices[j]:
-                break
-            
-        answer.append(seconds)
+    for i in range(len(prices)):
+        while stack != [] and stack[-1][1] > prices[i]:
+            past, _ = stack.pop()
+            answer[past] = i - past
+        stack.append([i, prices[i]])
+    
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
     
     return answer
