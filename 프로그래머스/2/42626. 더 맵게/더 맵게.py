2ca493f80547812af3ffd55e76a71097df8f18fd
Jiyun Kim <jyjyjy7418@gmail.com>
2025-01-22 21:52:51 +0900

    [level 2] Title: 더 맵게, Time: 988.01 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,16 +1,29 @@
-import heapq
+from collections import deque
 
 def solution(scoville, K):
-    heapq.heapify(scoville)
-
+    
+    scoville.sort()
+    scoville = deque(scoville)
+    queue = deque()
+    
     cnt = 0
-    while(scoville[0] < K):
-        if len(scoville) == 1:
+    while (scoville and scoville[0] < K) or (queue and queue[0] < K):
+        if len(scoville) + len(queue) < 2:
             return -1
         
-        m1 = heapq.heappop(scoville)
-        m2 = heapq.heappop(scoville)
-        heapq.heappush(scoville, m1 + m2*2)
+        m = [0, 0]
+        for i in range(2):
+            if scoville and queue:
+                if scoville[0] >= queue[0]:
+                    m[i] = queue.popleft()
+                else:
+                    m[i] = scoville.popleft()
+            elif scoville:
+                m[i] = scoville.popleft()
+            elif queue:
+                m[i] = queue.popleft()
+    
+        queue.append(m[0] + m[1] * 2)
         cnt += 1
     
     return cnt
