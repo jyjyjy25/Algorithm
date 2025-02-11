1cebd9148dd93d0e8ec0c7db2d23db517a09d101
Jiyun Kim <jyjyjy7418@gmail.com>
2025-02-11 20:52:10 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -4,12 +4,11 @@ N = int(sys.stdin.readline())
 K = int(sys.stdin.readline())
 
 graph = [[] for _ in range(N+1)]
-visited = [False] * (N+1)
-
 for _ in range(K):
     a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
+
 
 def DFS(v):
     visited[v] = True
@@ -17,7 +16,8 @@ def DFS(v):
     for g in graph[v]:
         if not visited[g]:
             DFS(g)
-    
 
+
+visited = [False] * (N+1)
 DFS(1)
-print(sum(visited)-1)
+print(sum(visited) - 1)
