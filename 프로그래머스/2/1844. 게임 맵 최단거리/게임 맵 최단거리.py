8b40ceba3716594e276c6d199ed790d4e339897c
Jiyun Kim <jyjyjy7418@gmail.com>
2025-02-14 23:52:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -5,26 +5,27 @@ def solution(maps):
     M = len(maps[0])
     
     visited = [[False] * M for _ in range(N)]
-    xlist = [0, 0, -1, 1]  # 상하좌우
-    ylist = [1, -1, 0, 0]
     
-    x, y = 0, 0  # 시작 좌표
-    queue = deque([(x, y)])
-    visited[x][y] = False
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
     
-    while queue:
-        x, y = queue.popleft()
+    def BFS(x, y):
+        queue = deque([(x, y)])
+        visited[x][y] = True
         
-        for dx, dy in zip(xlist, ylist):
-            nx = x + dx
-            ny = y + dy
-            if N > nx >= 0 and M > ny >= 0:
-                if maps[nx][ny] == 1 and not visited[nx][ny]:
-                    queue.append((nx, ny))
-                    visited[nx][ny] = False
-                    maps[nx][ny] = maps[x][y] + 1
-    
-    if maps[N-1][M-1] == 1:
-        return -1
-    else:
+        while queue:
+            x, y = queue.popleft()
+            for ax, ay in zip(dx, dy):
+                nx = x + ax
+                ny = y + ay
+                if N > nx >= 0 and M > ny >= 0:
+                    if maps[nx][ny] != 0 and not visited[nx][ny]:
+                        visited[nx][ny] = True
+                        queue.append((nx, ny))
+                        maps[nx][ny] = maps[x][y] + 1
+            
+    BFS(0, 0)
+    if maps[N-1][M-1] != 1:
         return maps[N-1][M-1]
+    else:
+        return -1
