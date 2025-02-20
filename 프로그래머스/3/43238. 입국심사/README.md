447fdb69672e41a0403ff65274905d60d3eb4b75
Jiyun Kim <jyjyjy7418@gmail.com>
2025-02-20 23:10:33 +0900

    [level 3] Title: 입국심사, Time: 570.33 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.10 ms
+메모리: 14.1 MB, 시간: 570.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 07일 00:36:24
+2025년 02월 20일 23:10:25
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,22 +1,17 @@
 def solution(n, times):
-    answer = 0
-
-    low = 1
+    low = 0
     high = max(times) * n
-    while (low<=high):
-        mid = (low+high) // 2
+    
+    while low <= high:
+        mid = (low + high) // 2
         
         cnt = 0
         for time in times:
             cnt += mid // time
         
-        if cnt >= n:
-            answer = mid
-            high = mid - 1
-        elif cnt < n:
+        if cnt < n:
             low = mid + 1
-            
-        print(low, high, mid, cnt)
-    
-    print(answer)
-    return answer
+        else:
+            high = mid - 1
+        
+    return low
