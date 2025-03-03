d9ea2c1a6b774ec5bac2ba79829e7f8721b5f2d1
Jiyun Kim <jyjyjy7418@gmail.com>
2025-03-03 18:26:42 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -4,25 +4,24 @@ N = int(sys.stdin.readline())
 
 stack = []
 for _ in range(N):
-    input_arr = list(sys.stdin.readline().split())
-    if len(input_arr) == 2:
-        if input_arr[0] == "push":
-            stack.append(input_arr[1])
-    elif len(input_arr) == 1:
-        if input_arr[0] == "pop":
-            if stack:
-                print(stack.pop())
-            else:
-                print(-1)
-        elif input_arr[0] == "size":
-            print(len(stack))
-        elif input_arr[0] == "empty":
-            if stack:
-                print(0)
-            else:
-                print(1)
-        elif input_arr[0] == "top":
-            if stack:
-                print(stack[-1])
-            else:
-                print(-1)
+    command = list(sys.stdin.readline().split())
+    
+    if command[0] == "push":
+        stack.append(command[1])
+    elif command[0] == "pop":
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == "top":
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
