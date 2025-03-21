50e571908681ec374fe05c82750c15a45b9e837c
Jiyun Kim <jyjyjy7418@gmail.com>
2025-03-22 00:50:34 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 124 ms, Memory: 40104 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,20 +1,24 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+arr = [[0 for _ in range(M+1)]]
+for _ in range(N):
+    temp_arr = [0]
+    arr.append(temp_arr + list(map(int, sys.stdin.readline().split())))
+
 K = int(sys.stdin.readline())
 find = [list(map(int, sys.stdin.readline().split())) for _ in range(K)]
 
-prefix_sum = [[0 for _ in range(M)] for _ in range(N)]
+prefix_sum = [[0 for _ in range(M+1)] for _ in range(N+1)]
 
-for i in range(N):
-    sum = 0
-    for j in range(M):
-        sum += arr[i][j]
-        prefix_sum[i][j] = sum
+for i in range(N+1):
+    for j in range(M+1):
+        if j == 0 and i == 0:
+            continue
+        elif j == 0:
+            prefix_sum[i][j] = prefix_sum[i-1][j]
+        else:
+            prefix_sum[i][j] = prefix_sum[i][j-1] + prefix_sum[i-1][j] - prefix_sum[i-1][j-1] + arr[i][j]
 
 for i, j, x, y in find:
-    temp = 0
-    for k in range(i, x+1):
-        temp += prefix_sum[k-1][y-1] - prefix_sum[k-1][j-1] + arr[k-1][j-1]
-    print(temp)
+    print(prefix_sum[x][y] - prefix_sum[x][j-1] - prefix_sum[i-1][y] + prefix_sum[i-1][j-1])
