e700da61f15d50e5c0ff2064890aaea0e90ae956
Jiyun Kim <jyjyjy7418@gmail.com>
2025-04-01 00:21:01 +0900

    [level 3] Title: 단속카메라, Time: 2.53 ms, Memory: 9.56 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -4,15 +4,15 @@ def solution(routes):
     answer = 0
     
     routes.sort()
-    prev_s, prev_e = routes[0][0], routes[0][1]
+    camera = routes[0][1]
     answer += 1
     
     record = []
     for s, e in routes[1:]:
-        if prev_e >= s:  # 두 차량의 경로가 겹침
-            prev_e = min(prev_e, e)
+        if camera >= s:  # 두 차량의 이동 경로가 겹침
+            camera = min(camera, e)
         else:
+            camera = e
             answer += 1
-            prev_e = e
 
     return answer
