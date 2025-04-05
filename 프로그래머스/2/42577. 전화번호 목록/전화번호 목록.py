0cfa3ded25173505aacdf9d2ac01b5b765ab9af8
Jiyun Kim <jyjyjy7418@gmail.com>
2025-04-05 22:24:12 +0900

    [level 2] Title: 전화번호 목록, Time: 168.55 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,17 +1,7 @@
-def solution(phone_book):
-    answer = True
-    
-#     phone_book.sort(key = lambda x: len(x))
-    
-#     for i in range(len(phone_book)):
-#         for j in range(i+1, len(phone_book)):
-#             if (phone_book[i] == phone_book[j][0:len(phone_book[i])]):
-#                 return False
-
+def solution(phone_book):    
     phone_book.sort()
-    
     for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][0:len(phone_book[i])]:
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
             return False
     
-    return answer
+    return True
