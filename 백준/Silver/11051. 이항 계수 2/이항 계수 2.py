f840d10329b1573ffd299b815442043cec32828e
Jiyun Kim <jyjyjy7418@gmail.com>
2025-04-11 00:32:18 +0900

    [Silver II] Title: 이항 계수 2, Time: 156 ms, Memory: 43664 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -7,9 +7,6 @@ for i in range(N+1):
 
 for i in range(2, N+1):
     for k in range(1, (i)//2+1):
-        if k == 1:
-            dp[i][k] = dp[i][i-1] = i % 10007
-        else:
-            dp[i][k] = dp[i][i-k] = (dp[i-1][k] + dp[i-1][k-1]) % 10007
+        dp[i][k] = dp[i][i-k] = (dp[i-1][k] + dp[i-1][k-1]) % 10007
 
 print(dp[N][K])
