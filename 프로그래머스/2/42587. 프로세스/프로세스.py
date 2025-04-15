0726b7c03cff901e03ddd81b64530f0f998634ec
Jiyun Kim <jyjyjy7418@gmail.com>
2025-04-15 20:18:15 +0900

    [level 2] Title: 프로세스, Time: 0.76 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,23 +1,21 @@
-# 19:53~
 from collections import deque
 
 def solution(priorities, location):
-    new_priorities = []
-    for i, p in enumerate(priorities):
-        new_priorities.append((p, i))
-    
-    priorities = deque(new_priorities)
-    order = 0
-    while priorities:
-        max_priority = priorities.popleft()
+    answer = 0
+
+    queue = deque([(p, i) for i, p in enumerate(priorities)])
+    while queue:
+        current_priority = queue.popleft()
         is_excute = True
-        for p in priorities:
-            if p[0] > max_priority[0]:
-                priorities.append(max_priority)
+        for p in queue:
+            if p[0] > current_priority[0]:
+                queue.append(current_priority)
                 is_excute = False
                 break
         
         if is_excute:
-            order += 1
-            if max_priority[1] == location:
-                return order
+            answer += 1
+            if current_priority[1] == location:
+                break
+    
+    return answer
