c8a0572e7bf13557f4f96506bffc216aa6f96c46
Jiyun Kim <jyjyjy7418@gmail.com>
2025-04-22 18:33:33 +0900

    [level 3] Title: 이중우선순위큐, Time: 3730.42 ms, Memory: 15.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,24 +1,39 @@
-import heapq as hq
+# 6:00~ 
+import heapq as hp
 
 def solution(operations):
-    min_queue = []  # 최소힙
-    max_queue = []  # 최대힙
+    max_heap, min_heap = [], []
+    hp.heapify(max_heap)
+    hp.heapify(min_heap)
     
     for op in operations:
-        command, data = op.split()
-        
-        if command == "I":
-            hq.heappush(min_queue, int(data))
-            hq.heappush(max_queue, -int(data))
-        elif command == "D":
-            if data == "-1" and min_queue:
-                temp = hq.heappop(min_queue)
-                max_queue.remove(-temp)
-            elif data == "1" and max_queue:
-                temp = hq.heappop(max_queue)
-                min_queue.remove(-temp)
+        ops = op.split()
+        if ops[0] == "I":
+            hp.heappush(min_heap, int(ops[1]))
+        elif ops[0] == "D":
+            if ops[1] == "1":
+                while min_heap:  # 최대힙으로 변경
+                    hp.heappush(max_heap, -hp.heappop(min_heap))
+                if max_heap: 
+                    hp.heappop(max_heap)
+            elif ops[1] == "-1":
+                while max_heap:  # 최소힙으로 변경
+                    hp.heappush(min_heap, -hp.heappop(max_heap))
+                if min_heap:
+                    hp.heappop(min_heap)
 
-    if min_queue or max_queue:
-        return [-hq.heappop(max_queue), hq.heappop(min_queue)]
-    else:
+    if not min_heap and not max_heap:
         return [0, 0]
+    else:
+        while max_heap:
+            hp.heappush(min_heap, -hp.heappop(max_heap))
+        min_value = hp.heappop(min_heap)
+
+        while min_heap:
+            hp.heappush(max_heap, -hp.heappop(min_heap))
+        if max_heap:
+            max_value = -hp.heappop(max_heap)
+        else:
+            max_value = min_value
+
+        return [max_value, min_value]
